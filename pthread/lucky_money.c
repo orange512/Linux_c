2d719d54fecdd554f14769f38bdbdfd3b94b0e68
orange <huiyi950512@gmail.com>
2017-09-22 22:14:18 +0800

    删除多余

--- pthread/lucky_money.c
+++ pthread/lucky_money.c
@@ -12,13 +12,17 @@
 #include<signal.h>
 #include<string.h>
 #include<time.h>
-int remainMoney;
-int left_MoneyPacksize;
-int RAND(int n,int m)
+int remainMoney;//剩余红包钱
+int left_MoneyPacksize;//剩余红包个数
+pthread_t g_id[20];
+pthread_cond_t cond;
+pthread_mutex_t lock;
+int getMoney;
+int RAND(int n,int m)//生成n-m之内的随机数
 {
     return rand()%(m-n+1)+n; 
 }
-int getRandomMoney()
+int getRandomMoney()//获取随机金额红包
 {
     if(remainMoney/left_MoneyPacksize == 1)
     {
@@ -36,56 +40,25 @@ int getRandomMoney()
     left_MoneyPacksize--;
     return money;
 }
-void destroy(pthread_t tid)
-{
-    printf("%destroy \n",(unsigned int)tid);
-    pthread_join(tid,0);
-}
-pthread_t mainid;
-pthread_t g_id[20];
-struct info{//个人信息
-    int id;
-    double m;
-    _Bool mark;//用于标记只能抢一次
-    char name[20];
-}person[20];
-void mutex_clean(void *mutex)
-{
-    pthread_mutex_unlock((pthread_mutex_t *)mutex);
-}
-pthread_cond_t cond;
-pthread_mutex_t lock;
+
 void* task(void *p)//抢红包
 {
     int n = (int)p;
     pthread_mutex_lock(&lock);
     //printf("%d准备好抢红包\n",n);
-    pthread_cleanup_push(mutex_clean,&lock);
     pthread_cond_wait(&cond,&lock);
-   // printf("剩余%d\n",remainMoney);
     if(remainMoney == 0)
     {
         pthread_mutex_unlock(&lock);
         printf("%d exit\n",n);
-        
-    //    pthread_mutex_unlock(&lock);
         pthread_exit(0);
-       // destroy(pthread_self());
     }
-    int getMoney = getRandomMoney();
-    //printf("线程%d取走%d.%d%d元\n",n,getMoney/100,(n/10)%10,n%10);
-    printf("线程%d取走%d分\n",n,getMoney);
-    //printf("剩余%d\n",remainMoney);
-   /* person[n].id = n;
-    person[n].m = getMoney;
-    person[n].mark = 1;*/
-    pthread_cleanup_pop(0);
+    getMoney = getRandomMoney();
+    printf("线程%d取走%d.%d%d元\n",n,getMoney/100,(getMoney/10)%10,getMoney%10);
     pthread_mutex_unlock(&lock);
     pthread_exit(0);
-//    destroy(pthread_self());
-
 }
-void * creat_money(void *p)//生产红包
+void* creat_money(void *p)//生产红包
 {
     remainMoney =(int)(*(double*)p * 100);
     pthread_mutex_lock(&lock);
@@ -100,9 +73,14 @@ void * creat_money(void *p)//生产红包
 
 int main()
 {
-    int n;
     int k;
     double user_money;
+    int err;
+    pthread_mutex_init(&lock,0);
+    pthread_cond_init(&cond,0);
+    pthread_t tid;
+    srand((unsigned)time(0));
+
     while(1)
     {
         printf("请输入红包金额和红包数量\n");
@@ -116,40 +94,20 @@ int main()
         printf("对不起，您的红包金额过少，请重新输入\n");
     }
 
-    int err;
-    for(int i = 0 ; i < 20;i++)//初始化
-    {
-        person[i].mark = 0;
-        person[i].id = -1;
-    }
-    pthread_mutex_init(&lock,0);
-    pthread_cond_init(&cond,0);
-    pthread_t tid;
-    char *buff;
 
-    srand((unsigned)time(0));
-
-    /*pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);*/
-    for(int i = 0 ; i  < 20;i++)
+    for(int i = 0 ; i  < 20;i++)//创建抢红包人数
     {
         if(err = pthread_create(&g_id[i],0,task,(void *)i)!= 0)
             printf("error");
     }
+
     pthread_create(&tid,0,creat_money,(void *)(&user_money));
-    pthread_join(tid,(void **)&buff);
-   // printf("%s\n",buff);
+    pthread_join(tid,0);
     for(int j = 0 ; j < 20;j++)
     {
         pthread_join(g_id[j],0);
-    //    printf("%djoin\n",j);
+   //    printf("%djoin\n",j);
     }
-   /* for(int i = 0 ;i < 20;i++)
-    {
-        if(person[i].mark == 1)
-        printf("id = %d\t money = %.2lf\n",person[i].id,person[i].m);
-    }*/
 
     pthread_cond_destroy(&cond);
     pthread_mutex_destroy(&lock);
