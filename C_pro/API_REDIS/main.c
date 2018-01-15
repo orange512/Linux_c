9dcd20e25d7a0dc9a6b3defe446f639d0095ffd1
orange512 <huiyi950512@gmail.com>
2018-01-15 20:13:48 +0800

    redis封装

---
+++ C_pro/API_REDIS/main.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年01月15日 星期一 18时35分36秒
+ ************************************************************************/
+
+#include"api_redis.h"
+
+int main()
+{
+    redisContext *c = connect_redisContext("127.0.0.1",6379);
+    set_redis(c,"set k1 hello");
+    printf("show_integer%d\n",show_integer(c,"strlen k1"));
+    char *value;    
+    get_redis(c,"get k1",&value);
+    printf("get k1 = %s",value);
+    free(value);
+    redis_free(c);
+}
